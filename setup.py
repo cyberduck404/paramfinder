24b071b5e18e4789de25493291dac391860800cd
master <cyberduck404@yandex.com>
2023-12-16 11:24:02 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='paramfinder',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/cyberduck404/paramfinder',
+    license='',
+    author='cyberduck404',
+    author_email='',
+    description=''
+)
