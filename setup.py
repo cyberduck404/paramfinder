fa42d953e4642f119fa7f81eeab5f7d8973b5ed7
master <cyberduck404@yandex.com>
2023-12-16 11:28:40 +0800

    fix

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='paramfinder',
     version='0.0.1',
-    packages=[''],
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'colorama'
+    ],
+    entry_points={
+        'console_scripts': [
+            'paramfinder = paramfinder.main:main'
+        ]
+    },
     url='https://github.com/cyberduck404/paramfinder',
-    license='',
+    license='MIT',
     author='cyberduck404',
     author_email='',
-    description=''
+    description='',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
 )
